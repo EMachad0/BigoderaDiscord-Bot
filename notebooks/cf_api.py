54c28f6fc0f2391a0886dd17b8b6d239824c5b6a
João Vitor Fröhlich <37422122+joao-frohlich@users.noreply.github.com>
2021-03-23 13:17:44 -0300

    Adicionando função para escolher problemas aleatórios

--- notebooks/cf_api.py
+++ notebooks/cf_api.py
@@ -1,6 +1,6 @@
 import urllib.request
 import json
-
+import random
 
 def get_codeforces_user_info(handles):
     param = ''
@@ -21,6 +21,42 @@ def get_codeforces_user_maxRank(handles):
         except KeyError as e:
             users[user['handle']] = {'maxRank': "unrated"}
     return users
+
+def get_codeforces_problem(handle, rating, tags):
+    all_tags = ['2-sat', 'binary search', 'bitmasks', 'brute force', 'chinese remainder theory',
+    'combinatorics','constructive algorithm','data strucutres','dfs and similar','divide and conquer',
+    'dp','dsu','expression parsing','fft','flows','games','geometry','graph matchings','graphs','greedy',
+    'hashing','implementations','interactive','math','matrices','meet-in-the-middle','number theory',
+    'probabilities','schedules','shortest paths','sortings','string suffix strucutures','strings',
+    'ternary search','trees','two pointers']
+    correct_tags = []
+    for tag in tags:
+        if tag in all_tags and tag not in correct_tags:
+            correct_tags.append(tag)
+    problems = []
+    solved = []
+    if handle != '':
+        with urllib.request.urlopen("https://codeforces.com/api/user.status?handle="+handle) as url:
+            codeforces_data = json.loads(url.read().decode())
+            for submission in codeforces_data['result']:
+                if 'verdict' in submission and submission['verdict'] == 'OK':
+                    solved.append([submission['problem']['contestId'],submission['problem']['index']])
+    with urllib.request.urlopen("https://codeforces.com/api/problemset.problems") as url:
+        codeforces_data = json.loads(url.read().decode())
+        for problem in codeforces_data['result']['problems']:
+            if 'rating' in problem and problem['rating'] == rating:
+                v = True
+                for tag in correct_tags:
+                    if not tag in problem['tags']:
+                        v = False
+                        break
+                if v:
+                    problemId = [problem['contestId'],problem['index']]
+                    if problemId not in solved:
+                        problems.append([problem['contestId'],problem['index']])
+    if len(problems) == 0: return 'No problem matched the parameters'
+    problem = problems[random.randint(0,len(problems)-1)]
+    return 'https://codeforces.com/contest/'+str(problem[0])+'/problem/'+str(problem[1])
 
 
 if __name__ == "__main__":
